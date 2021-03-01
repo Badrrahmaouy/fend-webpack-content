8a82b351e70d254ad63f42541661ab1b8d90841d
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-01 17:32:03 +0100

    added eslint plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    plugins: [new ESLintPlugin],
     module: {
         rules: [
             {
