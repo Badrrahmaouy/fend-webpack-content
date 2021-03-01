90c29cbf0eb058d083917f2e63e51fe179ed2ee4
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-01 20:22:58 +0100

    Added clean-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
@@ -6,6 +7,9 @@ module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    output: {
+        path: path.resolve(process.cwd(), 'dist')
+    },
     module: {
         rules: [
             {
@@ -20,6 +24,12 @@ module.exports = {
             template: './src/client/views/index.html',
             filename: './index.html',
             minify: 'false'
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
         })
     ]
 }
